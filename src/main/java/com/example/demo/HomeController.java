7752b33de16a7fd7aaa568350be79c913ecb89fc
seanliuboyds <seanliuboyds2010@gmail.com>
2018-08-23 16:40:26 -0400

    sjagsghdsoigh

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,32 +1,54 @@
 package com.example.demo;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Entity
+import javax.validation.Valid;
+
+@Controller
 public class HomeController {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    @Autowired
+    CourseRepository courseRepository;
 
-    @NotNull
-    @Size(min=4)
-    private String title;
-
-    @NotNull
-    @Size(min=3)
-    private String instructor;
-
-    @NotNull
-    @Size(min=10)
-    private String description;
-
-    @NotNull
-    @Min(3)
-    private int credit;
+    @RequestMapping("/")
+    public String listCourses(Model model){
+        model.addAttribute("courses", courseRepository.findAll());
+        return "list";
+    }
+    @GetMapping("/add")
+    public String courseForm(Model model){
+        model.addAttribute("course", new Course());
+        return "courseForm";
+    }
+    @PostMapping("/process")
+    public String processForm(@Valid Course course, BindingResult result)
+    {
+        if (result.hasErrors()){
+            return "courseForm";
+        }
+        courseRepository.save(course);
+        return "redirect:/";
+    }
+    @RequestMapping("/detail/{id}")
+    public String showCourse(@PathVariable("id") long id, Model model){
+    model.addAttribute("course", courseRepository.findById(id).get());
+    return "show";
+    }
+    @RequestMapping("/update/(id)")
+    public String updateCourse(@PathVariable("id") long id, Model model){
+        model.addAttribute("course", courseRepository.findById(id).get());
+        return "redirect:/";
+    }
+    @RequestMapping("/delete/(id)")
+    public String delCourse(@PathVariable("id") long id){
+        courseRepository.deleteById(id);
+        return "redirect:/";
+    }
 }
+
